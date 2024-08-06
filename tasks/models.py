abc43569c8a798fd0b335cb5f4faf72a20993580
mykyta <nikita.fedotov222@gmail.com>
2024-08-06 16:52:11 +0300

    create Worker(abstractuser) and task model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -15,4 +16,35 @@ class Position(models.Model):
         return self.name
 
 
+class Worker(AbstractUser):
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+        default=1
+    )
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}: {self.position}"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = {
+        "urgent": "Urgent",
+        "high": "High",
+        "medium": "Medium",
+        "low": "Low",
+    }
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=255,
+        choices=PRIORITY_CHOICES,
+        default="low"
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
