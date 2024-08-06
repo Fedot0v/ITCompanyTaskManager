8e3cd09eda73bc1f19ac415146c4052ba92791a6
mykyta <nikita.fedotov222@gmail.com>
2024-08-06 16:43:04 +0300

    create position model

--- tasks/models.py
+++ tasks/models.py
@@ -8,4 +8,11 @@ class TaskType(models.Model):
         return self.name
 
 
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 
