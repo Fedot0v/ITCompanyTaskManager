be920145cda3f7dd54dbd33c818fd350ef900948
mykyta <nikita.fedotov222@gmail.com>
2024-08-06 21:10:22 +0300

    add TaskListView with context object name task

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from tasks.models import Task
 
 
 def index(request):
+    """View function for the home page of the site."""
     return render(request, "index.html")
+
+
+class TaskListView(ListView):
+    model = Task
+    context_object_name = "task"
