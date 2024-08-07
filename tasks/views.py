d3edfc8e9b5ce8f416f1b5ca594f153eadb8690e
mykyta <nikita.fedotov222@gmail.com>
2024-08-07 21:22:56 +0300

    add context to index

--- tasks/views.py
+++ tasks/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView
 )
 
-from tasks.models import Task
+from tasks.models import Task, TaskType
 
 
 def index(request):
     """View function for the home page of the site."""
-    return render(request, "tasks/index.html")
+    #user = request.user
+    #num_tasks = Task.objects.filter(user=user).count()  # Фільтруємо за користувачем
+    #num_of_available_tasks = Task.objects.filter(user=user, is_completed=False).count()
+    #num_of_completed_tasks = Task.objects.filter(user=user, is_completed=True).count()
+    num_tasks = Task.objects.filter().count()  # Фільтруємо за користувачем
+    num_of_available_tasks = Task.objects.filter(is_completed=False).count()
+    num_of_completed_tasks = Task.objects.filter(is_completed=True).count()
+    context = {
+        "num_tasks": num_tasks,
+        "num_of_available_tasks": num_of_available_tasks,
+        "num_of_completed_tasks": num_of_completed_tasks,
+    }
+    return render(request, 'tasks/index.html', context)
 
 
 class TaskListView(ListView):
@@ -23,3 +36,18 @@ class TaskListView(ListView):
 class TaskCreateView(CreateView):
     model = Task
     fields = "__all__"
+    success_url = reverse_lazy("tasks:tasks-list")
+    template_name = "tasks/tasks_form.html"
+
+
+class TaskTypeListView(ListView):
+    model = TaskType
+    context_object_name = "task_types"
+
+
+class TaskTypeCreateView(CreateView):
+    model = TaskType
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tasks-list")
+    template_name = "tasks/tasktype_form.html"
+
