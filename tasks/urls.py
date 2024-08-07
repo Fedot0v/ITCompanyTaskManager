8d9889acd5928e6cc1b88bb00e48f8b9b7fbd71e
mykyta <nikita.fedotov222@gmail.com>
2024-08-07 18:09:43 +0300

    add tasks-list path and tasks-form path to create tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import index
+from .views import (
+    index,
+    TaskListView,
+    TaskCreateView
+)
 
-
-app_name = 'tasks'
+app_name = "tasks"
 
 urlpatterns = [
-    path('', index, name='index'),
+    path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="tasks-list"),
+    path("tasks/<int:pk>/create/", TaskCreateView.as_view(), name="tasks-form"),
 ]
