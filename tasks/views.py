6dbb45fdc92f0887009404f94631b1147ed00d96
mykyta <nikita.fedotov222@gmail.com>
2024-08-07 17:23:51 +0300

    add template_name and pagination to TaskListView

--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 
 from tasks.models import Task
 
 
 def index(request):
     """View function for the home page of the site."""
-    return render(request, "index.html")
+    return render(request, "tasks/index.html")
 
 
 class TaskListView(ListView):
     model = Task
-    context_object_name = "task"
+    context_object_name = "tasks"
+    template_name = "tasks/tasks_list.html"
+    paginated_by = 5
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = "__all__"
