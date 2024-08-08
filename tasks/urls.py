6451cfb57ec81be19b3c301d9f2735e3d8d58425
mykyta <nikita.fedotov222@gmail.com>
2024-08-08 13:52:57 +0300

    Removed <int:pk> from task creation url

--- tasks/urls.py
+++ tasks/urls.py
@@ -12,6 +12,6 @@ app_name = "tasks"
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="tasks-list"),
-    path("tasks/<int:pk>/create/", TaskCreateView.as_view(), name="tasks-form"),
+    path("tasks/create/", TaskCreateView.as_view(), name="tasks-form"),
     path("tasktype/<int:pk>/create/", TaskTypeCreateView.as_view(), name="tasktype-form"),
 ]
