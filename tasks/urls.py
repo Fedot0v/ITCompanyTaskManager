c1b75b184e782f4a74c93a63e71d511236db6f22
mykyta <nikita.fedotov222@gmail.com>
2024-08-09 17:57:04 +0300

    add new path for tasks of current user

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,11 @@ from .views import (
     index,
     TaskListView,
     TaskCreateView,
-    TaskTypeCreateView, TaskTypeListView, WorkerCreateView
+    TaskTypeCreateView,
+    TaskTypeListView,
+    WorkerCreateView,
+    TaskDetailView,
+    UserTaskListView
 )
 
 app_name = "tasks"
@@ -12,6 +16,8 @@ app_name = "tasks"
 urlpatterns = [
     path("", index, name="index"),
     path("tasks/", TaskListView.as_view(), name="tasks-list"),
+    path("mytasks/", UserTaskListView.as_view(), name="mytasks-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="tasks-detail"),
     path("tasks/create/", TaskCreateView.as_view(), name="tasks-form"),
     path("tasktype/create/", TaskTypeCreateView.as_view(), name="tasktype-form"),
     path("taskstype/", TaskTypeListView.as_view(), name="taskstype-list"),
