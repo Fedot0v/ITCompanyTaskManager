dc1a38f290ff4d335f19b7760153bce65393bea7
mykyta <nikita.fedotov222@gmail.com>
2024-08-18 15:23:50 +0300

    fix allowed hosts itcompanytaskmanager.onrender.com

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["127.0.0.1", "https://itcompanytaskmanager.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "itcompanytaskmanager.onrender.com"]
 
 # Application definition
 
