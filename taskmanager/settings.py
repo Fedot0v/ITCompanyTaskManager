99d029012438aab4aa806f9dba3ef097a4da2be9
mykyta <nikita.fedotov222@gmail.com>
2024-08-18 15:49:29 +0300

    remove import staticfile

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -14,7 +14,6 @@ from os import getenv
 from pathlib import Path
 
 import environ
-from django.contrib import staticfiles
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = "staticfiles/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
